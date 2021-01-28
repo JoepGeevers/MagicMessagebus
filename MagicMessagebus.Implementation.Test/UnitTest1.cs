85082fe5b902a8608c85d2807ad086d8125a66d7
Joep <joep@yourticketprovider.nl>
2021-01-28 20:09:14 +0100

    add solution and projects

---
+++ MagicMessagebus.Implementation.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace MagicMessagebus.Implementation.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
