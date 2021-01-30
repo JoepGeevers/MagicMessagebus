64264df1b89fb45bcc93604fffc8b385dc0d9936
Joep <joep@yourticketprovider.nl>
2021-01-30 21:30:18 +0100

    add SimpleStaticMagicMessagebusExample

---
+++ NinjectMagicMessagebusExample/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace NinjectMagicMessagebusExample
+{
+    using System;
+    using System.Threading;
+
+    using MagicMessagebus.Contract;
+    using MagicMessagebus.Implementation;
+    using Ninject;
+
+    class Program
+    {
+        static void Main()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Bind<IWakeUpSometimes>().To<ComputerProgram>().InSingletonScope();
+            kernel.Bind<IWriteToTheConsole>().To<IConsoleWriter>().InSingletonScope();
+            kernel.Bind<IMagicMessagebus>().To<MagicMessagebus>().InSingletonScope();
+
+            var waker = kernel.Get<IWakeUpSometimes>();
+
+            waker.WakeUp();
+
+            Thread.Sleep(100); // It's fire and forget, so we need the message to get through before the application finishes
+        }
+    }
+
+    public interface IWakeUpSometimes
+    {
+        void WakeUp();
+    }
+
+    public interface IWriteToTheConsole
+    {
+        void Subscribe(HelloWorld message);
+    }
+
+    public class ComputerProgram : IWakeUpSometimes
+    {
+        private readonly IMagicMessagebus messagebus;
+
+        public ComputerProgram(IMagicMessagebus messagebus)
+        {
+            this.messagebus = messagebus;
+        }
+
+        public void WakeUp()
+        {
+            var message = new HelloWorld
+            {
+                Body = "Hello, World!",
+            };
+
+            this.messagebus.Publish(message);
+        }
+    }
+
+    public class HelloWorld : IMagicMessage
+    {
+        public string Body { get; set; }
+    }
+
+    public class IConsoleWriter : IWriteToTheConsole
+    {
+        public void Subscribe(HelloWorld message)
+        {
+            Console.WriteLine(message.Body);
+        }
+    }
+}
