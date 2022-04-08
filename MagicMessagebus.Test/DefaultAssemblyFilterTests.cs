efd767fe5cac2486016c0f7e6973bd9c382f9035
Joep Geevers <joepgeevers@gmail.com>
2022-04-08 09:39:18 +0200

    add DefaultAssemblyFilterTests

---
+++ MagicMessagebus.Test/DefaultAssemblyFilterTests.cs
@@ -0,0 +1,41 @@
+﻿using MagicMessagebus.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MagicMessagebus.Test
+{
+    [TestClass]
+    public class DefaultAssemblyFilterTests
+    {
+        [TestMethod]
+        public void AssembliesThatStartWithMicrosoftShouldNotBeScanned()
+        {
+            // arrange
+            var assembly = Assembly.GetAssembly(typeof(Microsoft.Extensions.DependencyInjection.IServiceCollection));
+            var filter = new DefaultAssemblyFilter();
+
+            // act
+            var result = filter.ScanForSubcriptions(assembly);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void AssembliesThatStartWithMessagebusShouldBeScanned()
+        {
+            // arrange
+            var assembly = Assembly.GetAssembly(typeof(DefaultAssemblyFilter));
+            var filter = new DefaultAssemblyFilter();
+
+            // act
+            var result = filter.ScanForSubcriptions(assembly);
+
+            // assert
+            Assert.IsTrue(result);
+        }
+    }
+}
