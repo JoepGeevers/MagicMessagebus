358f3ef3b9b9ff571dfc9f2cf6eef210cd32cef9
Joep Geevers <joepgeevers@gmail.com>
2022-08-01 16:45:25 +0200

    remove assemblyversions

--- MagicMessagebus/Properties/AssemblyInfo.cs
+++ MagicMessagebus/Properties/AssemblyInfo.cs
@@ -1,8 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("MagicMessagebus.Test")]
-
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
-[assembly: AssemblyInformationalVersion("1.1.2")]
